735d23e2d8646d874637c771275e983efa638b4a
Ubuntu <ubuntu@ip-172-31-13-41.ap-northeast-2.compute.internal>
2022-08-12 01:08:59 +0000

    update chapter12

---
+++ RayHLi/chapter12/12.2/test.go
@@ -0,0 +1,7 @@
+package main
+import "fmt"
+
+func main() {
+	var test [3][2][5]float64
+	fmt.Println(len(test))  //external array size
+}
---
+++ RayHLi/chapter12/12.3/test.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func ChangeArray(arr [5]int) {
+	arr[3] = 3000
+}
+
+func main() {
+	a := [5]int{1,2,3,4,5}
+
+	ChangeArray(a)
+
+	fmt.Print(a[3])
+}
