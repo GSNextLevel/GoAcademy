2417ca0386e360144bf61a3cf5c398e603346c1f
viaSSH <hanseung0609@gmail.com>
2022-08-17 23:56:25 +0900

    chap5

---
+++ SSH/exercise/chap5/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a = 345
+	var b = 3.1415
+
+	fmt.Printf("%05d\n", a)
+	fmt.Printf("%5.2f\n", b)
+}
+
+// func main() {
+// 	var a int
+// 	var b int
+
+// 	fmt.Scanln(&a, &b)
+// 	fmt.Println(a, b)
+// }
